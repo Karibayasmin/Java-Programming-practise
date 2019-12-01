d31cb21ab5531d795479979f29d3e9390eef6da7
Kariba Yasmin <karibayasmin@gmail.com>
2019-12-01 17:10:23 +0600

    JavaOutputFormattingHackerRank edit

--- JavaOutputFormattingHackerRank.java
+++ JavaOutputFormattingHackerRank.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class JavaOutput FormattingHackerRank {
+public class JavaOutputFormattingHackerRank {
 
     public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
